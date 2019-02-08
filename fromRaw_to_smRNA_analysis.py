a7a69735ac3cfbe06b5e767cb4581f5c0b558b60
jfsanchez <jfbioinformatics@gmail.com>
2019-02-08 12:58:34 +0100

    Add mirtop UID

--- fromRaw_to_smRNA_analysis.py
+++ fromRaw_to_smRNA_analysis.py
@@ -538,7 +538,7 @@ def parse_gtf(gtffile, filename, sample):
 	
 	## Open file
 	fil = open(filename, 'w')    
-	string2write = 'type\tsample_name\tident\tname\tvariant\texpression\tseq\n'
+	string2write = 'type\tsample_name\tparent\tname\tvariant\tUID\tseq\texpression\n'
 	fil.write(string2write)
 	for line in lines:
 		if not line.startswith('#'):        
@@ -546,12 +546,12 @@ def parse_gtf(gtffile, filename, sample):
 			seq = line.split('\t')[-1].split(';')[0].split("Read=")[-1]
 			ident = line.split('\t')[0]
 			name = line.split('\t')[-1].split(';')[2].split("Name=")[-1]
+			UID = line.split('\t')[-1].split(';')[1].split("UID=")[-1]
 			parent = line.split('\t')[-1].split(';')[3].split("Parent=")[-1]
 			variant = line.split('\t')[-1].split(';')[4].split("Variant=")[-1]
-			expression = str(line.split('\t')[-1].split(';')[6].split("Expression=")[-1])			
-			string2write = 'isomiR\t' + sample + '\t' + ident + '\t' + name + '\t' + variant + '\t' + expression + '\t' + seq + '\n'
+			expression = str(line.split('\t')[-1].split(';')[6].split("Expression=")[-1])
+			string2write = 'isomiR\t' + sample + '\t' + ident + '\t' + name + '\t' + variant + '\t' + UID + '\t' + seq + '\t' + expression + '\n'
 			fil.write(string2write)
-			
 	fil.close()      
 ###############
 
