2f1f312b261f05f25d26ae1e862b9de5aa0895aa
jfsanchez <jfbioinformatics@gmail.com>
2019-09-09 12:38:54 +0200

    remodelling initiated

---
+++ XICRA/__init__.py
@@ -0,0 +1,16 @@
+#from pkg_resources import get_distribution
+#try:
+#    __version__ = get_distribution('XICRA').version
+#except:
+#    __version__ = 'local'
+### to include when distribution available
+## version will be retrieve from setup.py
+
+
+__all__ = [
+	'sampleParser',
+	'modules'
+]
+
+from XICRA import *
+
