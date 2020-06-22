257ca9b9761743383cd946cf01b7d9f3b0f45942
jfsanchez <jfsanchez@debian.jf>
2020-06-22 07:06:27 +0200

    updates

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import glob
+
+long_description_text = ""
+with open("README.md", "r") as fh:
+    long_description_text = fh.read()
+
+setuptools.setup(
+    name="XICRA",
+    version="0.2",
+
+    scripts=glob.glob('main/*'),
+    author="Jose F. Sanchez-Herrero",
+
+    author_email="jfbioinformatics@gmail.com",
+    description="",
+
+    long_description_content_type="text/markdown",
+    long_description=long_description_text,
+    url="https://github.com/JFsanchezherrero/XICRA",
+    packages=setuptools.find_packages(),
+    license='GPLv3',
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ]
+)
