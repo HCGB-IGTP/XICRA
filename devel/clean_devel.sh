9b91dd073b7d2613783d561d56beee40ca341559
jfsanchez <jfbioinformatics@gmail.com>
2020-06-22 10:38:07 +0200

    add devel pypi

---
+++ devel/clean_devel.sh
@@ -0,0 +1,9 @@
+echo "######################################"
+echo "## Clean old folders"
+echo "rm -r build"
+rm -r build
+echo "rm -r dist"
+rm -r dist
+echo "rm -r spaTyper.egg-info"
+rm -r XICRA.egg-info
+echo ""
---
+++ devel/create_distro.sh
@@ -0,0 +1,4 @@
+echo "######################################"
+echo "## Create new distribution"
+python setup.py sdist bdist_wheel
+echo ""
