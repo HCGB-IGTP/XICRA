89c8a0fea0453ee5c66545702d4c705e09bfb3cc
jfsanchez <jfsanchez@debian.jf>
2020-07-17 17:01:50 +0200

    update XICRA isomiR simulations

--- simulation/mod_freq.py
+++ simulation/mod_freq.py
@@ -15,19 +15,87 @@ from io import open
 import pandas as pd
 from collections import defaultdict
 import numpy as np
+import random
 
 ## import my modules
 from XICRA.scripts import functions
 
+## get frequencies
+def get_freq(given_df, col_list):
+    df_freq = pd.DataFrame()
+    for miRNA, row in given_df.iterrows():
+        for col in col_list:
+            df_freq.loc[miRNA, col] = row[col]/row['total']
+    return (df_freq)
 
 ## ARGV
 if len (sys.argv) < 3:
     print ("\nUsage:")
-    print ("python %s freq-table out_file\n" %os.path.realpath(__file__))
+    print ("python %s freq-table out_file debug_bool\n" %os.path.realpath(__file__))
     exit()
 
+## original counts
 original_counts = functions.get_data(sys.argv[1], ',', 'index_col=0')
-original_counts['total'] = original_counts.sum(axis=1)
+col_list = list(original_counts) ## get columns
+
+## add missing data
+modified_counts = original_counts.copy(deep=True)
+for col in col_list:
+    modified_counts.loc[modified_counts.sample(frac=0.35).index, col] = pd.np.nan
+
+## randomize 
+random_counts = modified_counts.apply(np.random.permutation, axis=1, result_type='broadcast')
+random_counts[np.isnan(random_counts)] = 0
+random_counts['total'] = random_counts.sum(axis=1)
+
+## get frequencies
+random_freqs = get_freq(random_counts, col_list)
+
+if (sys.argv[3]):
+    print ('##########')
+    print ('Counts')
+    print (original_counts)
+    print ('##########')
+    print ('')
+
+    print ('##########')
+    print ('Frequence')
+    original_counts['total'] = original_counts.sum(axis=1)
+    original_freq = get_freq(original_counts, col_list)
+    print (original_freq)
+    print ('##########')
+
+    ## print randomize counts & frequencies
+    print ('##########')
+    print ('Random Counts')
+    print (random_counts)
+    print ('##########')
+    print ('')
+
+    print ('##########')
+    print ('Frequence')
+    print (random_freqs)
+    print ('##########')
+
+## adjust to 100
+new_random = pd.DataFrame(columns=col_list)
+for miRNA, row in random_freqs.iterrows():
+    for col in col_list:
+        if row[col]==0:
+            new_random.loc[miRNA, col] = 0
+        else:
+            new_random.loc[miRNA, col] = int(row[col]*100) 
+
+new_random['total'] = new_random.sum(axis=1)
+for miRNA, row in new_random.iterrows():
+    if row['total']!=100:
+        sum = 100 - int(row['total']) 
+        rnd = random.sample(col_list, 1)
+        row[rnd] += sum
+
+new_random = new_random.drop(columns=['total'])
+new_random['total'] = new_random.sum(axis=1)
+
 
 print ('##########')
 print ('Counts')
@@ -35,22 +103,9 @@ print (original_counts)
 print ('##########')
 print ('')
 
-## get columns
-col_list = list(original_counts)
-col_list.remove('total')
-
-original_freq = pd.DataFrame()
-for miRNA, row in original_counts.iterrows():
-    for col in col_list:
-        original_freq.loc[miRNA, col] = row[col]/row['total']
-    
+## print randomize counts & frequencies
 print ('##########')
-print ('Frequence')
-print (original_freq)
+print ('Counts adjusted')
+print (new_random)
 print ('##########')
-
-df_random = original_freq.apply(np.random.permutation, axis=1, result_type='broadcast')
-print (df_random)
-print (type(df_random))
-
-
+print ('')
