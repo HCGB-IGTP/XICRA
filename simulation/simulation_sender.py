db0c7c4acfca2e0530c3befa3c790b68c4f92880
jfsanchez <jfsanchez@debian.jf>
2020-07-22 18:35:35 +0200

    fix simulation

--- simulation/simulation_sender.py
+++ simulation/simulation_sender.py
@@ -87,7 +87,7 @@ def NGS_simulator(name, abs_folder, seqSys_list, type_reads, fcov_list, fasta, t
                     outfile_path = os.path.join(tmp_fastq, outfile_name)
                     
                     ## command
-                    art_illumina_cmd = "%s -na -p -m 25 -s 5 -ss %s -i %s -l %s -f %s -o %s" %(art_illumina_bin, profile, fasta_file_len, str_len, fcov, outfile_path)
+                    art_illumina_cmd = "%s -na -p -m 50 -s 5 -ss %s -i %s -l %s -f %s -o %s" %(art_illumina_bin, profile, fasta_file_len, str_len, fcov, outfile_path)
                     
                     code = functions.system_call(art_illumina_cmd)
                     if not code:
