47ddec2da6032baee6eb9efbb306b99ca0dd378f
jfsanchez <jfsanchez@debian.jf>
2020-08-03 11:25:54 +0200

    update version; add requirements pip

--- XICRA_pip/setup.py
+++ XICRA_pip/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="XICRA",
-    version="0.9.0",
+    version="0.9.1",
 
     scripts=glob.glob('main/*'),
     author="Jose F. Sanchez-Herrero",
