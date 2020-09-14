78ba728944ce4d7c67d8e800d52f82c213979221
jfsanchezherrero <jfbioinformatics@gmail.com>
2020-09-14 12:12:06 +0200

    single-end fastqc call

--- XICRA_pip/XICRA/scripts/fastqc_caller.py
+++ XICRA_pip/XICRA/scripts/fastqc_caller.py
@@ -3,6 +3,7 @@
 ## Jose F. Sanchez                                      ##
 ## Copyright (C) 2019 Lauro Sumoy Lab, IGTP, Spain      ##
 ##########################################################
+from docutils.utils.math.math2html import file
 '''
 Calls FASTQC analysis and parses results generated.
 '''
@@ -20,26 +21,21 @@ from HCGB import functions
 from XICRA.config import set_config
 
 ############
-def call_fastqc(path, file1, file2, sample, fastqc_bin, threads):    
+def call_fastqc(path, files, sample, fastqc_bin, threads):    
     ## call system for fastqc sample given
     
-    ## check if previously done and succeeded
-    filename_stamp = path + '/.success'
-    if os.path.isfile(filename_stamp):
-        stamp = functions.time_functions.read_time_stamp(filename_stamp)
-        print (colored("\tA previous command generated results on: %s [%s -- %s]" %(stamp, name, 'fastqc'), 'yellow'))
-    else:
-        #name = functions.files_functions.create_subfolder(sample, path)
-        logFile = path + '/' + sample + '.log'
-        
-        ##print ("+ Calling fastqc for samples...")    
-        cmd_fastqc = '%s --extract -t %s -o %s %s %s > %s 2> %s' %(fastqc_bin, threads, path, file1, file2, logFile, logFile)
-        fastq_code = functions.system_call_functions.system_call( cmd_fastqc )
-        
-        if fastq_code:
-            functions.time_functions.print_time_stamp(filename_stamp)
-        else:
-            print ('** Sample %s failed...' %sample)
+    #name = functions.files_functions.create_subfolder(sample, path)
+    logFile = path + '/' + sample + '.log'
+    
+    # create string with files path
+    files_string = " ".join(files)
+    
+    ##print ("+ Calling fastqc for samples...")    
+    cmd_fastqc = '%s --extract -t %s -o %s %s > %s 2> %s' %(fastqc_bin, threads, path, files_string, logFile, logFile)
+    fastq_code = functions.system_call_functions.system_call( cmd_fastqc )
+    
+    if not fastq_code:
+        print ('** Sample %s failed...' %sample)
 
     ## send command    
     return (fastq_code)
@@ -47,15 +43,18 @@ def call_fastqc(path, file1, file2, sample, fastqc_bin, threads):
 ############
 def run_module_fastqc(path, files, sample, threads):    
     ## Arguments provided via ARGVs
-    fastqc_bin = set_config.get_exe('fastqc')
 
-    ## check if paired end
-    if (len(files) < 2):
-        print ('+ No implementation yet for single-end. Sorry.')
-        exit()
+    ## check if previously done and succeeded
+    filename_stamp = path + '/.success'
+    if os.path.isfile(filename_stamp):
+        stamp = functions.time_functions.read_time_stamp(filename_stamp)
+        print (colored("\tA previous command generated results on: %s [%s -- %s]" %(stamp, name, 'fastqc'), 'yellow'))
+    else:
+        ## call fastqc
+        fastqc_bin = set_config.get_exe('fastqc')
+        codeReturn = call_fastqc(path, files, sample, fastqc_bin, threads)
 
-    codeReturn = call_fastqc(path, files[0], files[1], sample, fastqc_bin, threads)
-    if codeReturn == 'FAIL':
-        exit()
-    path_to_sample = path + '/' + sample
-    return path_to_sample
+        if codeReturn:
+            functions.time_functions.print_time_stamp(filename_stamp)
+        
+        return ()
